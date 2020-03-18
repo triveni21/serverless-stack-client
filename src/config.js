8feb8f07d6581c3f8b9899a3065ada6412d2aae5
Triveni Badgujar <triveny21@gmail.com>
2020-03-18 15:55:20 +0530

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "test-my-serverless"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://5sdipl3ujl.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_BvbhHdfQr",
+    APP_CLIENT_ID: "2bnpmu43dpn3d3s1qs0upq5ahj",
+    IDENTITY_POOL_ID: "us-east-2:d0508d99-f471-4ccf-b7fc-72da44817fc8"
+  }
+};
